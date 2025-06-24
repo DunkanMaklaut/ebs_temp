591cdeab703ddcf7805657c53c16c869f7f57446
Sergey Stupin <seregstupin@gmail.com>
2025-06-24 23:28:34 +0600

    123

--- frontend/src/app/(mainPages)/profile/page.js
+++ frontend/src/app/(mainPages)/profile/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { NextPage } from "next";
+
 import Header from "@components/header/Header";
 import Breadcrumbs from "@components/breadcrumbs/breadcrumbs";
 import { lightTheme } from "@resources/colors/colors.js";
